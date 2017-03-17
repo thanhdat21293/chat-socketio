34c0a8ca88efc22590d398891c0bfde726167ac8
thanhdat21293 <thanhdat21293@2-04.local>
2017-03-17 14:56:40 +0700

    Chat group

--- chat.js
+++ chat.js
@@ -24,6 +24,9 @@ function handler(req, res) {
   });
 }
 
+
+
+
 let message = "";
 /***
  * Danh sách các message
@@ -32,17 +35,28 @@ let message = "";
  * join: Người dùng vào phòng
  */
 
+// usernames which are currently connected to the chat
+var usernames = {};
+
+// rooms which are currently available in chat
+var rooms = ['room1'];
+
 
 io.on('connection', (socket) => {
   //console.log(socket.id) // ID user khi load trang, mỗi lần load là 1 ID
-    /*
-  socket.on('welcome', (data) => {
-    //console.log(data);
-    socket.emit('welcome', {message: '<div class="title">Chào mừng bạn tham gia diễn đàn</div>'});
+
+  socket.on('switchRoom', function(newroom){
+      socket.leave(socket.room);
+      socket.join(newroom);
+      socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+      // sent message to OLD room
+      socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+      // update socket session room title
+      socket.room = newroom;
+      socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+      socket.emit('updaterooms', rooms, newroom);
   });
-  */
-    session.socket.id;
-    log(sess);
+
   //Don't use message name 'pong'. It is reserved message
   socket.on('chat', (data) => {
 
