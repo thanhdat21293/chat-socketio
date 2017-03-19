8e5568c546f5d218617c44b11b6143d8878d4bbb
thanh dat <thanh dat>
2017-03-19 23:55:27 +0700

    Chat group

--- chat.js
+++ chat.js
@@ -39,37 +39,65 @@ let message = "";
 var usernames = {};
 
 // rooms which are currently available in chat
-var rooms = ['room1'];
+var rooms = ['room1', 'room2'];
 
 
 io.on('connection', (socket) => {
   //console.log(socket.id) // ID user khi load trang, mỗi lần load là 1 ID
 
+
+  socket.on('join', function(username){
+        // Lưu username vào socket session
+        socket.username = username;
+
+        socket.room = 'room1';
+
+        usernames[username] = username;
+
+        socket.join('room1');
+
+        // echo to client they've connected
+        socket.emit('updatechat', 'Bạn', ' vào phòng room1');
+
+        // echo to room 1 that a person has connected to their room
+        socket.emit('updaterooms', rooms, 'room1');
+
+        //socket.broadcast.emit('join', data);
+    });
+
   socket.on('switchRoom', function(newroom){
       socket.leave(socket.room);
       socket.join(newroom);
-      socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+      socket.emit('updatechat', 'Bạn', ' vào phòng '+ newroom);
       // sent message to OLD room
-      socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+      socket.broadcast.to(socket.room).emit('updatechat', socket.username, ' thoát khỏi phòng');
       // update socket session room title
       socket.room = newroom;
-      socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+      socket.broadcast.to(newroom).emit('updatechat', socket.username, ' vào phòng');
       socket.emit('updaterooms', rooms, newroom);
   });
 
   //Don't use message name 'pong'. It is reserved message
   socket.on('chat', (data) => {
-
+        //log(io.sockets.sockets[socket]);
+    let name = socket.username;
     //console.log(data);
     //To broadcast, simply add a broadcast flag to emit and send method calls.
     //Broadcasting means sending a message to everyone else except for the socket that starts it.
-    socket.broadcast.emit('chat', data);
-  });
 
+      if(name) {
+          //socket.broadcast.emit('chat', name, data);
+          io.sockets.in(socket.room).emit('chat', socket.username, data);
+      }else{
+          socket.emit('nologin', data);
+      }
+     //io.sockets.in(socket.room).emit('chat', socket.username, data);
+  });
+/*
   socket.on('join', (data) => {
     socket.broadcast.emit('join', data);
   });
-
+*/
   socket.on('listuser', (data) => {
     socket.broadcast.emit('listuser', data);
   });
